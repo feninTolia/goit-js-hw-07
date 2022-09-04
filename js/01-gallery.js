651c9dff4c71fac3d017b64daa45d1b7a780b70a
feninTolia <fenintolja@gmail.com>
2022-09-04 11:52:01 +0300

    golden master

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+       <a class="gallery__link" href="${original}">
+         <img
+           class="gallery__image"
+           src="${preview}"
+           data-source="${original}"
+           alt="${description}"
+         />
+       </a>
+    </div>`
+  )
+  .join('');
+
+refs.gallery.innerHTML = galleryMarkup;
+
+refs.gallery.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  const onModalEscapeKeyDown = (evt) => {
+    if (evt.key === 'Escape') {
+      instance.close();
+      refs.gallery.removeEventListener('keydown', onModalEscapeKeyDown);
+    }
+  };
+
+  refs.gallery.addEventListener('keydown', onModalEscapeKeyDown);
+});
